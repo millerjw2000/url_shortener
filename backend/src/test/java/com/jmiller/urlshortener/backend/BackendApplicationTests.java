18c6d3449c8e4cbad4aff795d198e183188da221
millerjw2000 <millerjw2000@gmail.com>
2026-06-07 16:20:31 -0400

    backend init

---
+++ backend/src/test/java/com/jmiller/urlshortener/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmiller.urlshortener.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
