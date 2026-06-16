4e07722d9a90661755333b7aac13b4bf063c5da5
millerjw2000 <millerjw2000@gmail.com>
2026-06-16 11:23:42 -0400

    Url repo work

--- backend/src/main/java/com/jmiller/urlshortener/backend/Repo/UrlRepository.java
+++ backend/src/main/java/com/jmiller/urlshortener/backend/Repo/UrlRepository.java
@@ -7,6 +7,12 @@ import com.jmiller.urlshortener.backend.Model.Url;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.springframework.jdbc.core.RowMapper;
+
 @Repository
 public class UrlRepository {
     
@@ -17,6 +23,22 @@ public class UrlRepository {
     public void setTemplate(JdbcTemplate template) {
         this.template = template;
     }
+
+    private RowMapper<Url> mapper = new RowMapper<Url>() {
+        @Override
+        public Url mapRow(ResultSet rs, int row) throws SQLException {
+        
+            Url u = new Url();
+            
+            u.setId(rs.getString("id"));
+            u.setUserId(rs.getString("userId"));
+            u.setCode(rs.getString("code"));
+            u.setFullLink(rs.getString("fullLink"));
+
+            return u;
+
+        }
+    };
 
     public JdbcTemplate getTemplate(){
         return this.template;
@@ -30,6 +52,22 @@ public class UrlRepository {
             url.getFullLink(),
             url.getUserId()
         );
+    }
+
+    public ArrayList<Url> getAll(String userId) {
+        String query = "SELECT * FROM " + tableName + " WHERE userId = ?";
+        ArrayList<Url> urls = new ArrayList<Url>(template.query(query,mapper,userId));
+        return urls;
+    }
+
+    public String getFullLink(String code) {
+        String query = "SELECT fullLink from " + tableName + " WHERE code = ?";
+        return template.queryForObject(query,String.class,code);
+    }
+
+    public void delete(String id) {
+        String query = "DELETE FROM " + tableName + " WHERE id = ?";
+        template.update(query,id);
     }
 
 }
